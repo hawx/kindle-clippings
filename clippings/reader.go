6ebd4ab1c3b8a042d49f3bc83f21de69780cd47f
Joshua Hawxwell <m@hawx.me>
2015-05-18 20:15:51 +0100

    Initial commit

---
+++ clippings/reader.go
@@ -0,0 +1,144 @@
+// Package clippings provides a Reader for the Kindle's clipping format.
+package clippings
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	delim      = "=========="
+	dateFmt    = "Monday, 2 January 2006 15:04:05"
+	altDateFmt = "Monday, 2 January 06 15:04:05"
+)
+
+type Range struct {
+	From, To int
+}
+
+type Clipping struct {
+	Title    string
+	Author   string
+	Type     string
+	Page     *int   `json:",omitempty"`
+	Location *Range `json:",omitempty"`
+	Datetime time.Time
+	Content  string
+}
+
+type Reader struct {
+	*bufio.Reader
+}
+
+func NewReader(r io.Reader) *Reader {
+	return &Reader{
+		bufio.NewReader(r),
+	}
+}
+
+func (r *Reader) Read() (clipping Clipping, err error) {
+	// get title and author
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	lastP := strings.LastIndex(line, "(")
+	clipping.Title = line[:lastP-1]
+	clipping.Author = line[lastP+1 : len(line)-3]
+
+	// get type, pg, loc, datetime
+	line, err = r.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	// skip "- Your"
+	line = line[7:]
+
+	// get type
+	parts := strings.SplitN(line, " ", 2)
+	clipping.Type = parts[0]
+	line = parts[1]
+
+	for _, part := range strings.Split(line, "|") {
+		part = strings.TrimSpace(part)
+
+		if strings.HasPrefix(part, "Location") || strings.HasPrefix(part, "location") {
+			clipping.Location, _ = parseLocation(part[9:])
+
+		} else if strings.HasPrefix(part, "at location") {
+			clipping.Location, _ = parseLocation(part[12:])
+
+		} else if strings.HasPrefix(part, "on Page") || strings.HasPrefix(part, "on page") {
+			pg, _ := strconv.Atoi(part[8:])
+			clipping.Page = &pg
+
+		} else if strings.HasPrefix(part, "Added on") {
+			clipping.Datetime, _ = parseDatetime(part[9:])
+		}
+	}
+
+	// blank line
+	if _, err = r.ReadString('\n'); err != nil {
+		return
+	}
+
+	for {
+		line, err = r.ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		if line[:len(line)-2] == delim {
+			break
+		}
+
+		clipping.Content += line
+	}
+
+	clipping.Content = strings.TrimSpace(clipping.Content)
+
+	return
+}
+
+func (r *Reader) ReadAll() (clippings []Clipping, err error) {
+	for {
+		clipping, err := r.Read()
+		if err == io.EOF {
+			return clippings, nil
+		}
+		if err != nil {
+			return clippings, err
+		}
+		clippings = append(clippings, clipping)
+	}
+}
+
+func parseLocation(s string) (*Range, error) {
+	fields := strings.Split(s, "-")
+	switch len(fields) {
+	case 1:
+		loc, _ := strconv.Atoi(fields[0])
+		return &Range{loc, loc}, nil
+	case 2:
+		from, _ := strconv.Atoi(fields[0])
+		to, _ := strconv.Atoi(fields[1])
+		return &Range{from, to}, nil
+	}
+
+	return nil, fmt.Errorf("Not in range format: %v\n", s)
+}
+
+func parseDatetime(s string) (time.Time, error) {
+	t, err := time.Parse(dateFmt, s)
+	if err != nil {
+		return time.Parse(altDateFmt, s)
+	}
+
+	return t, nil
+}
---
+++ cmd.go
@@ -0,0 +1,70 @@
+// Kindle Clippings reads the clippings file from a Kindle and output them as
+// json.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"hawx.me/code/kindle-tools/kindle-clippings/clippings"
+)
+
+var (
+	onlyType = flag.String("only", "", "")
+)
+
+const helpMsg = `Usage: kindle-clippings PATH [--only TYPE]
+
+  Reads clippings from your Kindle and outputs them in json format to Stdout.
+
+ PATH
+     Path to Kindle, for example /media/johndoe/Kindle or /Volumes/Kindle.
+
+ --only TYPE
+     Only list items of the given type (Bookmark, Note or Highlight)
+`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println(helpMsg)
+	}
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Println(helpMsg)
+		return
+	}
+
+	path := filepath.Join(flag.Arg(0), "documents/My Clippings.txt")
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	r := clippings.NewReader(file)
+	items, err := r.ReadAll()
+	if err != nil {
+		fmt.Println("err: %v", err)
+		return
+	}
+
+	if *onlyType != "" {
+		var filtered []clippings.Clipping
+
+		for _, item := range items {
+			if item.Type == *onlyType {
+				filtered = append(filtered, item)
+			}
+		}
+
+		items = filtered
+	}
+
+	json.NewEncoder(os.Stdout).Encode(items)
+}
